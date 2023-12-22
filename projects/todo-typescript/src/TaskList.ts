626ee3f021130a1140c14f6726e94f5032a9f760
Kruze Zab <kruzezab4@gmail.com>
2023-12-22 11:42:08 +0545

    add search functionality to the task app

--- projects/todo-typescript/src/TaskList.ts
+++ projects/todo-typescript/src/TaskList.ts
@@ -1,23 +1,76 @@
 import { ITask } from './Task';
+import { createImage } from './utils';
+import listSvg from './assets/icons/list.svg';
+import circleOutline from './assets/icons/circle-outline.svg';
+import circleComplete from './assets/icons/complete.svg';
+
+export enum Filters {
+  ALL,
+  COMPLETE,
+  INCOMPLETE,
+  SEARCH,
+}
 
 interface ITaskList {
   list: ITask[];
+  filter: Filters;
   getTaskById: (id: string) => ITask | null;
   getTaskByIndex: (index: number) => ITask | null;
   addTask: (task: ITask) => ITask[];
   getTasks: () => ITask[];
+
+  renderEmptyList: () => void;
+  render: () => void;
 }
+
+const taskListElement = document.querySelector('.tasks') as HTMLDivElement;
 
 class TaskList implements ITaskList {
   list: ITask[];
+  filter: Filters;
+  query: string;
 
   /**
    * initialize list of tasks
    *
    * @param list list of tasks
    */
-  constructor(list: ITask[]) {
+  constructor(list: ITask[], filter: Filters) {
     this.list = list;
+    this.filter = filter;
+    this.query = '';
+  }
+
+  /**
+   * set the search query
+   *
+   */
+  setQuery(query: string) {
+    this.query = query;
+  }
+
+  /**
+   * get the search query
+   *
+   */
+  getQuery() {
+    return this.query;
+  }
+
+  /**
+   * Get the current filter
+   *
+   */
+  getFilter() {
+    return this.filter;
+  }
+
+  /**
+   * set the current filter
+   *
+   */
+  setFilter(filter: Filters) {
+    this.filter = filter;
   }
 
   getTaskById = (id: string) => {
@@ -45,6 +98,120 @@ class TaskList implements ITaskList {
   getTasks = () => {
     return this.list;
   };
+
+  renderEmptyList = () => {
+    const tasksEmpty = document.createElement('div');
+    tasksEmpty.classList.add('tasks-empty');
+
+    // <div class="tasks-empty__icon">
+    const emptyIcon = document.createElement('div');
+    emptyIcon.classList.add('tasks-empty__icon');
+
+    // svg icon of empty list
+    const svgIcon = createImage(listSvg);
+
+    emptyIcon.appendChild(svgIcon);
+
+    // Append empty icon to tasksEmpty
+    tasksEmpty.appendChild(emptyIcon);
+
+    const emptyText = document.createElement('p');
+    emptyText.classList.add('tasks-empty__text');
+    emptyText.textContent = 'You don\'t have any tasks yet. Create a new one.';
+
+    tasksEmpty.appendChild(emptyText);
+
+    taskListElement.appendChild(tasksEmpty);
+  };
+
+  render() {
+    taskListElement.innerHTML = '';
+    const currentFilter = this.getFilter();
+    let filteredTasks: ITask[] = [];
+
+    switch (currentFilter) {
+    case Filters.COMPLETE:
+      filteredTasks = this.list.filter((task) => task.completed);
+      break;
+    case Filters.INCOMPLETE:
+      filteredTasks = this.list.filter((task) => !task.completed);
+      break;
+    case Filters.SEARCH:
+      if (this.getQuery() === '') {
+        filteredTasks = this.list;
+      }
+      filteredTasks = this.list.filter((task) =>
+        task.title.toLowerCase().includes(this.getQuery()),
+      );
+      break;
+    default:
+      filteredTasks = this.list;
+      break;
+    }
+
+    if (filteredTasks.length === 0) {
+      this.renderEmptyList();
+      return;
+    }
+
+    filteredTasks.forEach((task) => {
+      // <div class="task">
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('task');
+      // <div class="task__content">
+      const content = document.createElement('div');
+      content.classList.add('task__content');
+
+      // <h3 class="task__title">${todo?.title}</h3>
+      const title = document.createElement('h3');
+      title.classList.add('task__title');
+      title.textContent = task.title;
+
+      // <p class="task__text">${todo?.description || ''}</p>
+      const description = document.createElement('p');
+      description.classList.add('task__text');
+
+      description.textContent = task.description || '';
+
+      // append to content
+      content.appendChild(title);
+      content.appendChild(description);
+
+      const actions = document.createElement('div');
+      // <button type="button" class="btn btn-icon btn-icon--secondary complete-btn" >
+      const toggleBtn = document.createElement('button');
+      toggleBtn.setAttribute('type', 'button');
+      toggleBtn.classList.add(
+        'btn',
+        'btn-icon',
+        'btn-icon--secondary',
+        'complete-btn',
+      );
+
+      // Button Icon
+      let btnIcon;
+      if (task.getCompleted()) {
+        btnIcon = createImage(circleComplete);
+      } else {
+        btnIcon = createImage(circleOutline);
+      }
+
+      // Append the SVG to the compelte button
+      toggleBtn.appendChild(btnIcon);
+
+      // Add event listener to the toggle btn
+      toggleBtn.addEventListener('click', () => {
+        task.toggleCompleted();
+        this.render();
+      });
+
+      actions.appendChild(toggleBtn);
+      wrapper.appendChild(content);
+      wrapper.appendChild(actions);
+
+      taskListElement.appendChild(wrapper);
+    });
+  }
 }
 
 export default TaskList;
--- projects/todo-typescript/src/main.ts
+++ projects/todo-typescript/src/main.ts
@@ -1,19 +1,8 @@
 import './sass/main.scss';
-import Task, { ITask } from './Task';
-import TaskList from './TaskList';
-import circleOutline from './assets/icons/circle-outline.svg';
-import circleComplete from './assets/icons/complete.svg';
-import listSvg from './assets/icons/list.svg';
-import { createImage } from './utils';
-
-enum Filters {
-  ALL,
-  COMPLETE,
-  INCOMPLETE,
-}
+import Task from './Task';
+import TaskList, { Filters } from './TaskList';
 
 // Get DOM Elements
-const taskListElement = document.querySelector('.tasks') as HTMLDivElement;
 // Modals
 const modal = document.querySelector('#modal') as HTMLDivElement;
 const modalOpener = document.querySelectorAll('.modal-opener');
@@ -22,14 +11,16 @@ const taskForm = document.querySelector('#add-task') as HTMLFormElement;
 const modalTaskForm = document.querySelector(
   '#modal-add-form',
 ) as HTMLFormElement;
+
+const searchInput = document.querySelector('#search') as HTMLInputElement;
+
 // Filter btns
 const allFilterBtns = document.querySelectorAll('.filter-all');
 const completeFilterBtns = document.querySelectorAll('.filter-complete');
 const incompleteFilterBtns = document.querySelectorAll('.filter-incomplete');
 
 // Intitialize variables
-const taskList = new TaskList([]);
-let currentFilter = Filters.ALL;
+const taskList = new TaskList([], Filters.ALL);
 
 /**
  * create a new task instance and
@@ -44,106 +35,6 @@ function createTask(title: string, description: string, completed: boolean) {
   const task = new Task(title, description, completed);
   taskList.addTask(task);
   return task;
-}
-
-/**
- * render the task into the DOM
- *
- * @param tasks Task object list
- */
-function renderTasks(tasks: ITask[]) {
-  if (!taskListElement) throw new Error('Container not found!');
-
-  taskListElement.innerHTML = '';
-
-  if (tasks.length === 0) {
-    // Render empty list
-
-    // <div class="tasks-empty">
-    const tasksEmpty = document.createElement('div');
-    tasksEmpty.classList.add('tasks-empty');
-
-    // <div class="tasks-empty__icon">
-    const emptyIcon = document.createElement('div');
-    emptyIcon.classList.add('tasks-empty__icon');
-
-    // svg icon of empty list
-    const svgIcon = createImage(listSvg);
-
-    emptyIcon.appendChild(svgIcon);
-
-    // Append empty icon to tasksEmpty
-    tasksEmpty.appendChild(emptyIcon);
-
-    const emptyText = document.createElement('p');
-    emptyText.classList.add('tasks-empty__text');
-    emptyText.textContent = "You don't have any tasks yet. Create a new one.";
-
-    tasksEmpty.appendChild(emptyText);
-
-    taskListElement.appendChild(tasksEmpty);
-    return;
-  }
-
-  tasks.forEach((task) => {
-    // <div class="task">
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('task');
-    // <div class="task__content">
-    const content = document.createElement('div');
-    content.classList.add('task__content');
-
-    // <h3 class="task__title">${todo?.title}</h3>
-    const title = document.createElement('h3');
-    title.classList.add('task__title');
-    title.textContent = task.title;
-
-    // <p class="task__text">${todo?.description || ''}</p>
-    const description = document.createElement('p');
-    description.classList.add('task__text');
-
-    description.textContent = task.description || '';
-
-    // append to content
-    content.appendChild(title);
-    content.appendChild(description);
-
-    const actions = document.createElement('div');
-    // <button type="button" class="btn btn-icon btn-icon--secondary complete-btn" >
-    const toggleBtn = document.createElement('button');
-    toggleBtn.setAttribute('type', 'button');
-    toggleBtn.classList.add(
-      'btn',
-      'btn-icon',
-      'btn-icon--secondary',
-      'complete-btn',
-    );
-
-    // Button Icon
-    let btnIcon;
-    if (task.getCompleted()) {
-      btnIcon = createImage(circleComplete);
-    } else {
-      btnIcon = createImage(circleOutline);
-    }
-
-    // Append the SVG to the compelte button
-    toggleBtn.appendChild(btnIcon);
-
-    // Add event listener to the toggle btn
-    toggleBtn.addEventListener('click', () => {
-      task.toggleCompleted();
-      renderFilteredTasks();
-    });
-
-    actions.appendChild(toggleBtn);
-    wrapper.appendChild(content);
-    wrapper.appendChild(actions);
-
-    taskListElement.appendChild(wrapper);
-
-    return;
-  });
 }
 
 /**
@@ -164,33 +55,11 @@ function handleTaskSubmit(event: SubmitEvent) {
 
   // create task and render it on the page
   createTask(title, description, completed);
-  renderTasks(taskList.list);
+
+  taskList.render();
   // reset the form
   taskForm.reset();
   handleModalClose();
-}
-
-/**
- * Render tasks based on the current filter
- */
-function renderFilteredTasks() {
-  let filteredTasks: ITask[];
-
-  switch (currentFilter) {
-    case Filters.COMPLETE:
-      filteredTasks = taskList.list.filter((task) => task.completed);
-
-      break;
-    case Filters.INCOMPLETE:
-      filteredTasks = taskList.list.filter((task) => !task.completed);
-
-      break;
-    default:
-      filteredTasks = taskList.list;
-      break;
-  }
-
-  renderTasks(filteredTasks);
 }
 
 /**
@@ -198,7 +67,7 @@ function renderFilteredTasks() {
  *
  * @param filter The selected filter
  */
-function handleFilterClick(filter: Filters) {
+function handleFilterClick(currentFilter: Filters) {
   // Remove active class from the buttons
   allFilterBtns.forEach((btn) => {
     btn.classList.remove('header__menu-link--active');
@@ -212,28 +81,8 @@ function handleFilterClick(filter: Filters) {
     btn.classList.remove('header__menu-link--active');
   });
 
-  currentFilter = filter;
-
-  // Add active class to the selected button
-  switch (currentFilter) {
-    case Filters.COMPLETE:
-      completeFilterBtns.forEach((btn) =>
-        btn.classList.add('header__menu-link--active'),
-      );
-      break;
-    case Filters.INCOMPLETE:
-      incompleteFilterBtns.forEach((btn) =>
-        btn.classList.add('header__menu-link--active'),
-      );
-      break;
-    default:
-      allFilterBtns.forEach((btn) =>
-        btn.classList.add('header__menu-link--active'),
-      );
-      break;
-  }
-
-  renderFilteredTasks();
+  taskList.setFilter(currentFilter);
+  taskList.render();
 }
 
 /**
@@ -280,25 +129,33 @@ function setupEventListeners() {
   allFilterBtns.forEach((allFilterBtn) => {
     allFilterBtn.addEventListener('click', () => {
       handleFilterClick(Filters.ALL);
+      allFilterBtn.classList.add('header__menu-link--active');
     });
   });
 
   completeFilterBtns.forEach((completeFilterBtn) => {
-    completeFilterBtn.addEventListener('click', () =>
-      handleFilterClick(Filters.COMPLETE),
-    );
+    completeFilterBtn.addEventListener('click', () => {
+      handleFilterClick(Filters.COMPLETE);
+      completeFilterBtn.classList.add('header__menu-link--active');
+    });
   });
 
   incompleteFilterBtns.forEach((incompleteFilterBtn) => {
-    incompleteFilterBtn.addEventListener('click', () =>
-      handleFilterClick(Filters.INCOMPLETE),
-    );
+    incompleteFilterBtn.addEventListener('click', () => {
+      handleFilterClick(Filters.INCOMPLETE);
+      incompleteFilterBtn.classList.add('header__menu-link--active');
+    });
+  });
+
+  // Handle when user searches
+  searchInput.addEventListener('input', () => {
+    taskList.setQuery(searchInput.value);
+    taskList.setFilter(Filters.SEARCH);
+    taskList.render();
   });
 }
 
 // Call functions
-createTask('Task title', 'Task description', false);
-
-renderTasks(taskList.list);
+taskList.render();
 
 setupEventListeners();
